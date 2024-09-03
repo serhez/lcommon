294082f9aab39ef0a09089c31389c4c0fe2625ac
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-09-03 18:57:10 +0100

    feat(threading): execute a function safely guarded by a lock

--- lcommon/utils/threading.py
+++ lcommon/utils/threading.py
@@ -12,3 +12,27 @@ class GuardedValue:
 
     lock: threading.Lock = threading.Lock()
     """The lock to be acquired before accessing the value."""
+
+
+def safe_exec_with_lock(lock: threading.Lock, func, *args, **kwargs):
+    """
+    Execute a function with a lock acquired.
+
+    ### Parameters
+    --------------
+    `lock`: the lock to be acquired.
+    `func`: the function to be executed.
+    `*args`: the positional arguments to be passed to the function.
+    `**kwargs`: the keyword arguments to be passed to the function.
+
+    ### Returns
+    --------------
+    The return value of the function.
+    """
+
+    lock.acquire()
+    try:
+        res = func(*args, **kwargs)
+    finally:
+        lock.release()
+    return res
